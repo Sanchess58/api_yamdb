404e5ebe8a3d703da772ca4742e13d38540b2e25
Татьяна Белянина <b.tannya.d@gmail.com>
2023-05-11 22:54:31 +0300

    Write models Category, Genre, Title, GenreTitle

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    category = models.ForeignKey(Category,
+                                 null=True,
+                                 on_delete=models.SET_NULL,
+                                 related_name='titles',
+                                 verbose_name='Категория')
+    genre = models.ManyToManyField(Genre,
+                                   through='GenreTitle',
+                                   verbose_name='Жанр')
+    description = models.TextField(verbose_name='Описание',
+                                   null=True,
+                                   blank=True)
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name='Произведение',
+        on_delete=models.CASCADE)
+    genre = models.ForeignKey(
+        Genre,
+        verbose_name='Жанр',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title, self.genre
+
